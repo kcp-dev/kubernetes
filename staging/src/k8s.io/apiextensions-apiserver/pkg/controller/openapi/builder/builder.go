kcp-dev/kubernetes

commit 92e378346654bf7611ed82b8c4529ae534e61a63

HACK: Enable adding legacy scheme resources as CRDs...
This may be useful in case when some legacy scheme resources
are not registered, which is the case for example in KCP.

There has to be special cases for `core` resources that
have an empty group and are at a dedicated api prefix.

This doesn't fully work with `kubectl` or kubernetes client,
due to the fact that those client tools systematically use
strategic merge patch for legacy scheme resources, and CRDs
don't support strategic merge patch for now.
The fix for this limitation will be in a next commit.

Signed-off-by: David Festal <dfestal@redhat.com>

staging/src/k8s.io/apiextensions-apiserver/pkg/controller/openapi/builder/builder.go
@@ -45,6 +45,7 @@ import (
 	openapibuilder "k8s.io/kube-openapi/pkg/builder"
 	"k8s.io/kube-openapi/pkg/common"
 	"k8s.io/kube-openapi/pkg/util"
+	"k8s.io/kubernetes/pkg/api/legacyscheme"
 )
 
 const (
@@ -142,11 +143,17 @@ func BuildSwagger(crd *apiextensionsv1.CustomResourceDefinition, version string,
 	scale := &v1.Scale{}
 
 	routes := make([]*restful.RouteBuilder, 0)
-	root := fmt.Sprintf("/apis/%s/%s/%s", b.group, b.version, b.plural)
+	// HACK: support the case when we add core resources through CRDs (KCP scenario)
+	rootPrefix := fmt.Sprintf("/apis/%s/%s", b.group, b.version)
+	if b.group == "" {
+		rootPrefix = fmt.Sprintf("/api/%s", b.version)
+	}
+
+	root := fmt.Sprintf("%s/%s", rootPrefix, b.plural)
 
 	if b.namespaced {
 		routes = append(routes, b.buildRoute(root, "", "GET", "list", "list", sampleList).Operation("list"+b.kind+"ForAllNamespaces"))
-		root = fmt.Sprintf("/apis/%s/%s/namespaces/{namespace}/%s", b.group, b.version, b.plural)
+		root = fmt.Sprintf("%s/namespaces/{namespace}/%s", rootPrefix, b.plural)
 	}
 	routes = append(routes, b.buildRoute(root, "", "GET", "list", "list", sampleList))
 	routes = append(routes, b.buildRoute(root, "", "POST", "post", "create", sample).Reads(sample))
@@ -195,9 +202,21 @@ type CRDCanonicalTypeNamer struct {
 	kind    string
 }
 
+// HACK: support the case when we add core or other legacy scheme resources through CRDs (KCP scenario)
+func packagePrefix(group string) string {
+	if !strings.Contains(group, ".") &&
+		legacyscheme.Scheme.IsGroupRegistered(group) {
+		if group == "" {
+			group = "core"
+		}
+		return "k8s.io/api/" + group
+	}
+	return group
+}
+
 // OpenAPICanonicalTypeName returns canonical type name for given CRD
 func (c *CRDCanonicalTypeNamer) OpenAPICanonicalTypeName() string {
-	return fmt.Sprintf("%s/%s.%s", c.group, c.version, c.kind)
+	return fmt.Sprintf("%s/%s.%s", packagePrefix(c.group), c.version, c.kind)
 }
 
 // builder contains validation schema and basic naming information for a CRD in
@@ -452,7 +471,7 @@ func addTypeMetaProperties(s *spec.Schema) {
 
 // buildListSchema builds the list kind schema for the CRD
 func (b *builder) buildListSchema() *spec.Schema {
-	name := definitionPrefix + util.ToRESTFriendlyName(fmt.Sprintf("%s/%s/%s", b.group, b.version, b.kind))
+	name := definitionPrefix + util.ToRESTFriendlyName(fmt.Sprintf("%s/%s/%s", packagePrefix(b.group), b.version, b.kind))
 	doc := fmt.Sprintf("List of %s. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md", b.plural)
 	s := new(spec.Schema).WithDescription(fmt.Sprintf("%s is a list of %s", b.listKind, b.kind)).
 		WithRequired("items").
@@ -489,14 +508,19 @@ func (b *builder) getOpenAPIConfig() *common.Config {
 		GetOperationIDAndTags: openapi.GetOperationIDAndTags,
 		GetDefinitionName: func(name string) (string, spec.Extensions) {
 			buildDefinitions.Do(buildDefinitionsFunc)
+			// HACK: support the case when we add core or other legacy scheme resources through CRDs (KCP scenario)
+			parts := strings.Split(name, "/")
+			if len(parts) == 2 {
+				name = packagePrefix(parts[0])
+			}
 			return namer.GetDefinitionName(name)
 		},
 		GetDefinitions: func(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 			def := generatedopenapi.GetOpenAPIDefinitions(ref)
-			def[fmt.Sprintf("%s/%s.%s", b.group, b.version, b.kind)] = common.OpenAPIDefinition{
+			def[fmt.Sprintf("%s/%s.%s", packagePrefix(b.group), b.version, b.kind)] = common.OpenAPIDefinition{
 				Schema: *b.schema,
 			}
-			def[fmt.Sprintf("%s/%s.%s", b.group, b.version, b.listKind)] = common.OpenAPIDefinition{
+			def[fmt.Sprintf("%s/%s.%s", packagePrefix(b.group), b.version, b.listKind)] = common.OpenAPIDefinition{
 				Schema: *b.listSchema,
 			}
 			return def
@@ -505,6 +529,8 @@ func (b *builder) getOpenAPIConfig() *common.Config {
 }
 
 func newBuilder(crd *apiextensionsv1.CustomResourceDefinition, version string, schema *structuralschema.Structural, v2 bool) *builder {
+	group := crd.Spec.Group
+	// HACK: support the case when we add core resources through CRDs (KCP scenario)
 	b := &builder{
 		schema: &spec.Schema{
 			SchemaProps: spec.SchemaProps{Type: []string{"object"}},
@@ -512,7 +538,7 @@ func newBuilder(crd *apiextensionsv1.CustomResourceDefinition, version string, s
 		listSchema: &spec.Schema{},
 		ws:         &restful.WebService{},
 
-		group:    crd.Spec.Group,
+		group:    group,
 		version:  version,
 		kind:     crd.Spec.Names.Kind,
 		listKind: crd.Spec.Names.ListKind,

pkg/controlplane/server.go
@@ -27,11 +27,13 @@ import (
 	"strings"
 	"time"
 
+	"github.com/emicklei/go-restful"
 	extensionsapiserver "k8s.io/apiextensions-apiserver/pkg/apiserver"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	"k8s.io/apiserver/pkg/authorization/union"
+	"k8s.io/apiserver/pkg/endpoints/discovery"
 	openapinamer "k8s.io/apiserver/pkg/endpoints/openapi"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
 	genericapiserver "k8s.io/apiserver/pkg/server"
@@ -111,6 +113,18 @@ func CreateServerChain(completedOptions completedServerRunOptions, stopCh <-chan
 		// we don't need special handling for innerStopCh because the aggregator server doesn't create any go routines
 		return nil, err
 	}
+
+	// HACK: support the case when we can add core or other legacy scheme resources through CRDs (KCP scenario)
+	// In such a case, the request should be processed by the CRD handler
+	// (registered in the apiExtensionsServer NonGoRestfulMux handler)
+	// and not by the main KubeAPIServer.
+	kubeAPIServer.GenericAPIServer.Handler.GoRestfulContainer.Filter(func(req *restful.Request, res *restful.Response, chain *restful.FilterChain) {
+		if discovery.IsAPIContributed(req.Request.URL.Path) {
+			apiExtensionsServer.GenericAPIServer.Handler.NonGoRestfulMux.ServeHTTP(res.ResponseWriter, req.Request)
+		} else {
+			chain.ProcessFilter(req, res)
+		}
+	})
 
 	return aggregatorServer, nil
 }

staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/validation/validation.go
@@ -30,6 +30,7 @@ import (
 	utilvalidation "k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/apiserver/pkg/util/webhook"
+	"k8s.io/kubernetes/pkg/api/legacyscheme"
 
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -48,7 +49,11 @@ var (
 func ValidateCustomResourceDefinition(obj *apiextensions.CustomResourceDefinition, requestGV schema.GroupVersion) field.ErrorList {
 	nameValidationFn := func(name string, prefix bool) []string {
 		ret := genericvalidation.NameIsDNSSubdomain(name, prefix)
-		requiredName := obj.Spec.Names.Plural + "." + obj.Spec.Group
+		group := obj.Spec.Group
+		if group == "" {
+			group = "core"
+		}
+		requiredName := obj.Spec.Names.Plural + "." + group
 		if name != requiredName {
 			ret = append(ret, fmt.Sprintf(`must be spec.names.plural+"."+spec.group`))
 		}
@@ -171,8 +176,12 @@ func validateCustomResourceDefinitionVersion(version *apiextensions.CustomResour
 func validateCustomResourceDefinitionSpec(spec *apiextensions.CustomResourceDefinitionSpec, opts validationOptions, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
-	if len(spec.Group) == 0 {
-		allErrs = append(allErrs, field.Required(fldPath.Child("group"), ""))
+	// HACK: Relax naming constraints when registering legacy schema resources through CRDs
+	// for the KCP scenario
+	if legacyscheme.Scheme.IsGroupRegistered(spec.Group) {
+		// No error: these are legacy schema kubernetes types
+		// that are not added in the controlplane schema
+		// and that we want to move up to the KCP as CRDs
 	} else if errs := utilvalidation.IsDNS1123Subdomain(spec.Group); len(errs) > 0 {
 		allErrs = append(allErrs, field.Invalid(fldPath.Child("group"), spec.Group, strings.Join(errs, ",")))
 	} else if len(strings.Split(spec.Group, ".")) < 2 {

staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/apiserver.go
@@ -206,6 +206,8 @@ func (c completedConfig) New(delegationTarget genericapiserver.DelegationTarget)
 	}
 	s.GenericAPIServer.Handler.NonGoRestfulMux.Handle("/apis", crdHandler)
 	s.GenericAPIServer.Handler.NonGoRestfulMux.HandlePrefix("/apis/", crdHandler)
+	// HACK: Added to allow serving core resources registered through CRDs (for the KCP scenario)
+	s.GenericAPIServer.Handler.NonGoRestfulMux.HandlePrefix("/api/v1/", crdHandler)
 
 	crdController := NewDiscoveryController(s.Informers.Apiextensions().V1().CustomResourceDefinitions(), versionDiscoveryHandler, groupDiscoveryHandler)
 	namingController := status.NewNamingConditionController(s.Informers.Apiextensions().V1().CustomResourceDefinitions(), crdClient.ApiextensionsV1())

staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_discovery_controller.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"k8s.io/klog"
+	"k8s.io/kubernetes/pkg/api/legacyscheme"
 
 	autoscaling "k8s.io/api/autoscaling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -104,11 +105,18 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 			// If there is any Served version, that means the group should show up in discovery
 			foundGroup = true
 
+			// HACK: support the case when we add core resources through CRDs (KCP scenario)
+			groupVersion := crd.Spec.Group + "/" + v.Name
+			if crd.Spec.Group == "" {
+				groupVersion = v.Name
+			}
+
 			gv := metav1.GroupVersion{Group: crd.Spec.Group, Version: v.Name}
+
 			if !versionsForDiscoveryMap[gv] {
 				versionsForDiscoveryMap[gv] = true
 				apiVersionsForDiscovery = append(apiVersionsForDiscovery, metav1.GroupVersionForDiscovery{
-					GroupVersion: crd.Spec.Group + "/" + v.Name,
+					GroupVersion: groupVersion,
 					Version:      v.Name,
 				})
 			}
@@ -167,30 +175,46 @@ func (c *DiscoveryController) sync(version schema.GroupVersion) error {
 		}
 	}
 
+	sortGroupDiscoveryByKubeAwareVersion(apiVersionsForDiscovery)
+
+	resourceListerFunc := discovery.APIResourceListerFunc(func() []metav1.APIResource {
+		return apiResourcesForDiscovery
+	})
+
+	// HACK: if we are adding resources in legacy scheme group through CRDs (KCP scenario)
+	// then do not expose the CRD `APIResource`s in their own CRD-related group`,
+	// But instead add them in the existing legacy schema group
+	if legacyscheme.Scheme.IsGroupRegistered(version.Group) {
+		if !foundGroup || !foundVersion {
+			delete(discovery.ContributedResources, version)
+		}
+
+		discovery.ContributedResources[version] = resourceListerFunc
+	}
+
 	if !foundGroup {
 		c.groupHandler.unsetDiscovery(version.Group)
 		c.versionHandler.unsetDiscovery(version)
 		return nil
 	}
 
-	sortGroupDiscoveryByKubeAwareVersion(apiVersionsForDiscovery)
+	if version.Group != "" {
+		// If we don't add resources in the core API group
+		apiGroup := metav1.APIGroup{
+			Name:     version.Group,
+			Versions: apiVersionsForDiscovery,
+			// the preferred versions for a group is the first item in
+			// apiVersionsForDiscovery after it put in the right ordered
+			PreferredVersion: apiVersionsForDiscovery[0],
+		}
+		c.groupHandler.setDiscovery(version.Group, discovery.NewAPIGroupHandler(Codecs, apiGroup))
 
-	apiGroup := metav1.APIGroup{
-		Name:     version.Group,
-		Versions: apiVersionsForDiscovery,
-		// the preferred versions for a group is the first item in
-		// apiVersionsForDiscovery after it put in the right ordered
-		PreferredVersion: apiVersionsForDiscovery[0],
+		if !foundVersion {
+			c.versionHandler.unsetDiscovery(version)
+			return nil
+		}
+		c.versionHandler.setDiscovery(version, discovery.NewAPIVersionHandler(Codecs, version, resourceListerFunc))
 	}
-	c.groupHandler.setDiscovery(version.Group, discovery.NewAPIGroupHandler(Codecs, apiGroup))
-
-	if !foundVersion {
-		c.versionHandler.unsetDiscovery(version)
-		return nil
-	}
-	c.versionHandler.setDiscovery(version, discovery.NewAPIVersionHandler(Codecs, version, discovery.APIResourceListerFunc(func() []metav1.APIResource {
-		return apiResourcesForDiscovery
-	})))
 
 	return nil
 }

staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_handler.go
@@ -46,6 +46,7 @@ import (
 	"k8s.io/apiextensions-apiserver/pkg/crdserverscheme"
 	"k8s.io/apiextensions-apiserver/pkg/registry/customresource"
 	"k8s.io/apiextensions-apiserver/pkg/registry/customresource/tableconvertor"
+	"k8s.io/kubernetes/pkg/api/legacyscheme"
 
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -257,6 +258,10 @@ func (r *crdHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 	crdName := requestInfo.Resource + "." + requestInfo.APIGroup
+	// HACK: support the case when we add core resources through CRDs (KCP scenario)
+	if requestInfo.APIGroup == "" {
+		crdName = crdName + "core"
+	}
 	crd, err := r.crdLister.Get(crdName)
 	if apierrors.IsNotFound(err) {
 		if !r.hasSynced() {
@@ -330,6 +335,9 @@ func (r *crdHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	supportedTypes := []string{
 		string(types.JSONPatchType),
 		string(types.MergePatchType),
+	}
+	if legacyscheme.Scheme.IsGroupRegistered(requestInfo.APIGroup) {
+		supportedTypes = append(supportedTypes, string(types.StrategicMergePatchType))
 	}
 	if utilfeature.DefaultFeatureGate.Enabled(features.ServerSideApply) {
 		supportedTypes = append(supportedTypes, string(types.ApplyPatchType))
@@ -764,12 +772,16 @@ func (r *crdHandler) getOrCreateServingInfoFor(uid types.UID, name string) (*crd
 			table,
 		)
 
+		selfLinkPrefixPrefix := path.Join("apis", crd.Spec.Group, v.Name)
+		if crd.Spec.Group == "" {
+			selfLinkPrefixPrefix = path.Join("api", v.Name)
+		}
 		selfLinkPrefix := ""
 		switch crd.Spec.Scope {
 		case apiextensionsv1.ClusterScoped:
-			selfLinkPrefix = "/" + path.Join("apis", crd.Spec.Group, v.Name) + "/" + crd.Status.AcceptedNames.Plural + "/"
+			selfLinkPrefix = "/" + selfLinkPrefixPrefix + "/" + crd.Status.AcceptedNames.Plural + "/"
 		case apiextensionsv1.NamespaceScoped:
-			selfLinkPrefix = "/" + path.Join("apis", crd.Spec.Group, v.Name, "namespaces") + "/"
+			selfLinkPrefix = "/" + selfLinkPrefixPrefix + "/namespaces/"
 		}
 
 		clusterScoped := crd.Spec.Scope == apiextensionsv1.ClusterScoped

staging/src/k8s.io/apiextensions-apiserver/pkg/controller/status/naming_controller.go
@@ -368,6 +368,10 @@ func (c *NamingConditionController) deleteCustomResourceDefinition(obj interface
 
 func (c *NamingConditionController) requeueAllOtherGroupCRDs(name string) error {
 	pluralGroup := strings.SplitN(name, ".", 2)
+	// In case the group is empty because we're adding core resources as CRDs in KCP
+	if len(pluralGroup) == 1 {
+		pluralGroup = append(pluralGroup, "")
+	}
 	list, err := c.crdLister.List(labels.Everything())
 	if err != nil {
 		return err

staging/src/k8s.io/apiextensions-apiserver/pkg/registry/customresource/validator.go
@@ -131,7 +131,12 @@ func (a customResourceValidator) ValidateTypeMeta(ctx context.Context, obj *unst
 	if typeAccessor.GetKind() != a.kind.Kind {
 		allErrs = append(allErrs, field.Invalid(field.NewPath("kind"), typeAccessor.GetKind(), fmt.Sprintf("must be %v", a.kind.Kind)))
 	}
-	if typeAccessor.GetAPIVersion() != a.kind.Group+"/"+a.kind.Version {
+	// HACK: support the case when we add core resources through CRDs (KCP scenario)
+	expectedAPIVersion := a.kind.Group + "/" + a.kind.Version
+	if a.kind.Group == "" {
+		expectedAPIVersion = a.kind.Version
+	}
+	if typeAccessor.GetAPIVersion() != expectedAPIVersion {
 		allErrs = append(allErrs, field.Invalid(field.NewPath("apiVersion"), typeAccessor.GetAPIVersion(), fmt.Sprintf("must be %v", a.kind.Group+"/"+a.kind.Version)))
 	}
 	return allErrs

commit 0fd4b2f6bd311b2d8995f9d363beec8c17835cac

HACK: Fix of a bug introduced in commit 92e378346654bf7611ed82b8c4529ae534e61a63
This bug was leading to erroneous CRD OpenAPIV2 generation for CRDs that
are not part of the legacy scheme.

Signed-off-by: David Festal <dfestal@redhat.com>

staging/src/k8s.io/apiextensions-apiserver/pkg/controller/openapi/builder/builder.go
@@ -509,11 +509,6 @@ func (b *builder) getOpenAPIConfig() *common.Config {
 		GetOperationIDAndTags: openapi.GetOperationIDAndTags,
 		GetDefinitionName: func(name string) (string, spec.Extensions) {
 			buildDefinitions.Do(buildDefinitionsFunc)
-			// HACK: support the case when we add core or other legacy scheme resources through CRDs (KCP scenario)
-			parts := strings.Split(name, "/")
-			if len(parts) == 2 {
-				name = packagePrefix(parts[0])
-			}
 			return namer.GetDefinitionName(name)
 		},
 		GetDefinitions: func(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {